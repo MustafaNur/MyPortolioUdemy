f0761a0139ab74ccd28d6de9d15308094b2461e9
MustafaNur <mustafa@drupart.co.uk>
2024-05-06 14:20:59 +0300

    Istatistikler & Mesajlar geliştirildi

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy.DAL.Context;
+
+namespace MyPortolioUdemy.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyProtfolioContext context = new MyProtfolioContext();
+        public IActionResult Inbox()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+        public IActionResult ChangeIsReadToTrue(int id)
+        {
+            var value = context.Messages.Find(id);
+            value.MessageIsRead= true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult ChangeIsReadToFalse(int id)
+        {
+            var value = context.Messages.Find(id);
+            value.MessageIsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = context.Messages.Find(id);
+            context.Messages.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult MessageDetail(int id)
+        {
+            var value = context.Messages.Find(id);
+            return View(value);
+        }
+    }
+}
---
+++ Controllers/StatisticController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy.DAL.Context;
+
+namespace MyPortolioUdemy.Controllers
+{
+    public class StatisticController : Controller
+    {
+        MyProtfolioContext context = new MyProtfolioContext();
+        public IActionResult Index()
+        {
+            ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Count();
+            ViewBag.v3 = context.Messages.Where(x=>x.MessageIsRead==false).Count();
+            ViewBag.v4 = context.Messages.Where(x=>x.MessageIsRead==true).Count();
+            return View();
+        }
+    }
+}
