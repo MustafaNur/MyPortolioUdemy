4b87069331c78020d5bf10f14d45acf3e4d7cc08
MustafaNur <mustafa@drupart.co.uk>
2024-05-07 19:25:29 +0300

    todolist & notification

--- Migrations/20240506183718_mig2.Designer.cs
+++ Migrations/20240506183718_mig2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPortolioUdemy.DAL.Context;
 
@@ -11,9 +12,10 @@ using MyPortolioUdemy.DAL.Context;
 namespace MyPortolioUdemy.Migrations
 {
     [DbContext(typeof(MyProtfolioContext))]
-    partial class MyProtfolioContextModelSnapshot : ModelSnapshot
+    [Migration("20240506183718_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
+++ ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy.DAL.Context;
 
 namespace MyPortolioUdemy.ViewComponents.LayoutViewComponents
 {
 	public class _LayoutNavbarComponentPartial:ViewComponent
 	{
+		MyProtfolioContext context = new MyProtfolioContext();
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			ViewBag.toDoListcount = context.ToDoLists.Where(x => x.Status == false).Count();
+			var values = context.ToDoLists.Where(x => x.Status == false).ToList();
+			return View(values);
 		}
 	}
 }
